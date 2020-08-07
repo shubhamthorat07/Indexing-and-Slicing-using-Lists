a99ff3ca18efb2c6e8b7dde5be97f251c65706eb
Shubham <40167163+shubhamthorat07@users.noreply.github.com>
2020-08-07 20:52:45 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,23 @@
+l = [1,2,3,4,5, "Html", "Css", "Java"]
+print(l[0])
+print(l[1])
+print(l[2])
+
+print(l[-1])
+print(l[-2])
+print(l[::2])
+
+
+
+
+
+l1 = ["Javascript", "Python", [10,20,30,40]]
+print(l1[0])
+print(l1[-1])
+print(l1[-1][0])
+print(l1[-1][2])
+print(l1[::2])
+
+
+
+
